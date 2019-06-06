900218e8eba008acd04bad4e683b0b124f62962a
y-kazuya <y.kazuya.0124@au.com>
2019-06-06 20:31:03 +0900

    Google

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  API_KEY = "AIzaSyAu--Z2zo37olt65ga_kQ2rJLBzNT_u6Ys"
 
   def index
 
@@ -7,7 +8,7 @@ class PagesController < ApplicationController
   def summary
     #期間は3ヶ月
     ekimei = "新宿"
-    category = 1
+    category = 2
     kyroi = 3
 
     #今日から3ヶ月前までのPRを取得
@@ -16,19 +17,39 @@ class PagesController < ApplicationController
 
     @result = []
 
-    2.times do |n|
+    3.times do |n|
       uri = URI.parse("https://9b199e13.prtimes.tech/date_period/#{from}/#{today}/#{n + 1}")
       json = Net::HTTP.get(uri) #NET::HTTPを利用してAPIを叩く
+
+
       JSON.parse(json)["data"].each do |pr|
-        @result << pr
+
+        url = URI.parse("https://9b199e13.prtimes.tech/detail/#{pr['company_id'].to_i}/#{pr['release_id'].to_i}/")
+        json = Net::HTTP.get(url)
+        if (JSON.parse(json)["data"]["address"] != [] || JSON.parse(json)["data"]["address"][0] != nil)  && JSON.parse(json)["data"]["main_category_id"] == category
+          @result << JSON.parse(json)["data"]
+        end
+      end
+    end #@result 期間（三ヶ月以内と、住所があるかどうか、カテゴリー一致してるかどうかで絞ってる
+
+
+    @finish = []
+
+    @result.each do |pr|
+      if pr["address"][0] != nil
+
+        address = pr["address"][0].gsub(" ", "")
+        uri = Addressable::URI.parse("https://maps.googleapis.com/maps/api/directions/json?origin=#{ekimei}&destination=#{address}&key=#{API_KEY}")
+        json = Net::HTTP.get(uri)
+
+        if kyroi < JSON.parse(json)["routes"][0]["legs"][0]["distance"]["value"]
+            pr["distance"] = JSON.parse(json)["routes"][0]["legs"][0]["distance"]
+            @finish << pr
+        end
       end
     end
 
-
-    @result
-
-
-    # https://9b199e13.prtimes.tech/date_period/2019-01-22/2019-05-30/111 期間絞り　半年で 1000Pくらい
+    @finish
 
   end
 end
