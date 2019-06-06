dddb99e5f464ee3c30a786878d4b2bc018bcf23e
y-kazuya <y.kazuya.0124@au.com>
2019-06-06 17:25:06 +0900

    Ll

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,32 @@ class PagesController < ApplicationController
   end
 
   def summary
+    #期間は3ヶ月
     ekimei = "新宿"
     category = 1
     kyroi = 3
 
+    #今日から3ヶ月前までのPRを取得
+    today = Date.today
+    from = Time.current.ago(3.month).to_s.split(" ")[0]
 
-    aaa = 3
-    uri = URI.parse("https://9b199e13.prtimes.tech/main_category_release/001/4")
-    json = Net::HTTP.get(uri) #NET::HTTPを利用してAPIを叩く
-    @result = JSON.parse(json) #返ってきたjsonデータをrubyの配列に変換
+    @result = []
 
+    3.times do |n|
+      uri = URI.parse("https://9b199e13.prtimes.tech/date_period/#{from}/#{today}/#{n + 1}")
+
+      @a =
+      json = Net::HTTP.get(uri) #NET::HTTPを利用してAPIを叩く
+      @a = JSON.parse(json)["data"].each do |pr|
+        @result << pr
+      end
+    end
+
+
+    @result
+
+
+    # https://9b199e13.prtimes.tech/date_period/2019-01-22/2019-05-30/111 期間絞り　半年で 1000Pくらい
 
   end
 end
